a5ab3e18057ce0c2f211935730678c9da39e4050
wwwgzpucom <admin@gzpu.com>
2026-03-30 21:35:35 +0800

    修复 AppDelegate.swift 语法错误，添加应用图标

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -1,3 +1,4 @@
+
 import UIKit
 import Capacitor
 
@@ -41,9 +42,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
         // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
+        return true
     }
 
 }
